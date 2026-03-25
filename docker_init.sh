237a693816c75c3ae1b5c133a7d05ed28ca498df
Luca Verardo <luca.verardo@accenture.com>
2026-03-25 11:13:41 +0000

    Updated Docker image

--- docker_init.sh
+++ docker_init.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -ex
+
+# Substitute environment variables in the compiled Angular JS bundle.
+envsubst  < static_angular/assets/env.template.js > "static_angular/assets/env.js"
+
+# Run Alembic database migrations
 flask db upgrade
-python -m app run --host=0.0.0.0
+
+# Start nginx in the background
+nginx
+
+# Start Flask API server in the foreground (nginx proxies /api/ and /ws/ to it)
+python -m app run --host=0.0.0.0 --port="${PORT:-5051}"
